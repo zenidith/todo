69eb83bd3a388c817c838130208729428c26da40
K Sheenan <kaynesheenan+git@gmail.com>
2021-08-19 00:28:07 +0900

    started JS

---
+++ app.js
@@ -0,0 +1,16 @@
+const form = document.getElementById('form');
+const input = document.getElementById('input');
+const todosUL = document.getElementById('todos');
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    addTodo();
+})
+
+function addTodo(todo){
+    let todoText = input.value;
+    if(todo){
+        todoText = todo.text;
+    }
+    console.log(todoText);
+}
